e86701e96c83a9f27f16b9ced32741baecf5248b
parkwj93 <parkwj93@ajou.ac.kr>
2017-12-08 17:56:10 +0900

    sms/ pemission설정

--- app/src/main/java/com/example/taewoonglim/nusobo/LoginActivity.java
+++ app/src/main/java/com/example/taewoonglim/nusobo/LoginActivity.java
@@ -107,10 +107,17 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
     private void requestSmsPermission() {
         String permission = android.Manifest.permission.READ_SMS;
+        String permission2 =android.Manifest.permission.INTERNET;
+        String permission3= android.Manifest.permission.READ_EXTERNAL_STORAGE;
+        String permission4=android.Manifest.permission.GET_ACCOUNTS;
+        //String permission5=android.Manifest.permission.
         int grant = ContextCompat.checkSelfPermission(this, permission);
         if (grant != PackageManager.PERMISSION_GRANTED) {
-            String[] permission_list = new String[1];
+            String[] permission_list = new String[4];
             permission_list[0] = permission;
+            permission_list[1]=permission2;
+            permission_list[2]=permission3;
+            permission_list[3]=permission4;
             ActivityCompat.requestPermissions(this, permission_list, 1);
         }
     }
@@ -167,6 +174,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = result.getSignInAccount();
                 firebaseAuthWithGoogle(account);
+
             } else {
                 // Google Sign In failed, update UI appropriately
                 // ...
--- app/src/main/java/com/example/taewoonglim/nusobo/sms.java
+++ app/src/main/java/com/example/taewoonglim/nusobo/sms.java
@@ -22,32 +22,49 @@ public class sms extends BroadcastReceiver {
         String body="";
         String sender;
         Date curDate;
+        boolean flag=false;
         if (bundle != null) {
             Object[] smsExtra = (Object[]) bundle.get("pdus");
             msgs = new SmsMessage[smsExtra.length];
 
             for (int i = 0; i < msgs.length; i++) {
                 SmsMessage sms = SmsMessage.createFromPdu((byte[]) smsExtra[i]);
-               body = sms.getMessageBody().toString();
+                body = sms.getMessageBody().toString();
                 sender = sms.getOriginatingAddress().toString();
                 curDate =new Date(sms.getTimestampMillis());
                 Toast.makeText(context, "From123 :" + sender + "\n" + "body:" + body, Toast.LENGTH_LONG).show();
+                if(sender.equals("01076255866")){flag=true;}
+                //String year2=Integer.toString(curDate.getYear());
+                //Toast.makeText(context, "year    " + year2, Toast.LENGTH_SHORT).show();
             }
-            StringTokenizer tokenizer = new StringTokenizer(body, "\n");
-            tokenizer.nextToken();
-            tokenizer.nextToken();
-            tokenizer.nextToken();
-            tokenizer.nextToken();
-            String amount = tokenizer.nextToken();
-            amount = amount.trim();
-            amount = amount.replace(",", "");
-            amount = amount.replace("원", "");
-            Toast.makeText(context,"amount"+amount, Toast.LENGTH_SHORT).show();
-            String store=tokenizer.nextToken();
-            StringTokenizer tokenizer2= new StringTokenizer(store, " ");
-            String store2=tokenizer2.nextToken();
-            Toast.makeText(context,"store"+store2, Toast.LENGTH_SHORT).show();
-            Toast.makeText(context,"전화번호 ㅇㅋ", Toast.LENGTH_SHORT).show();
-        }
+            if(flag) {
+
+                StringTokenizer tokenizer = new StringTokenizer(body, "\n");
+                tokenizer.nextToken();
+                tokenizer.nextToken();
+                tokenizer.nextToken();
+                String cal = tokenizer.nextToken();
+                StringTokenizer tokenizer3 = new StringTokenizer(cal, "/");
+                String month = tokenizer3.nextToken();
+                String month2=tokenizer3.nextToken();
+                StringTokenizer tokenizer4 = new StringTokenizer(month2, " ");
+                String day = tokenizer4.nextToken();
+                String amount = tokenizer.nextToken();
+                amount = amount.trim();
+                amount = amount.replace(",", "");
+                //
+                amount = amount.replace("원", "");
+
+                Toast.makeText(context, "amount" + amount, Toast.LENGTH_SHORT).show();
+
+                String store = tokenizer.nextToken();
+                StringTokenizer tokenizer2 = new StringTokenizer(store, " ");
+                //아래꺼가 돈어디서 썻는지
+                String store2 = tokenizer2.nextToken();
+                Toast.makeText(context, "store" + store2, Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "month" + month, Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "day" + day, Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "전화번호 ㅇㅋ", Toast.LENGTH_SHORT).show();
+            }}
     }
 }
